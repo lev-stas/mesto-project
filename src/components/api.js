bdf1d64289182b96f8aee661f2b069814e9b7f58
Stanislav Levchenko <staslevchenko@protonmail.com>
2022-04-19 10:53:53 +0300

    Added functionality to edit profile info on backend

---
+++ src/components/api.js
@@ -0,0 +1,66 @@
+//Load cards from Server
+import {
+    profileTitle,
+    profileSubtitle
+} from "./vars.js";
+
+import { fillProfile } from "./profile.js";
+
+
+export function getCards() {
+    fetch('https://nomoreparties.co/v1/plus-cohort-9/cards', {
+            method: 'GET',
+            headers: {
+                authorization: 'c74b18e0-4a10-4f3c-8fc2-aaf6d9f451dc'
+            }
+        })
+        .then((res) => {
+            if (res.ok) {
+                return res.json()
+            } else {
+                return res.status
+            }
+        })
+        .then((data) => {
+            console.log(data);
+        });
+
+};
+
+
+//Profile info
+
+export function getProfileRequest() {
+    const promise = fetch('https://nomoreparties.co/v1/plus-cohort-9/users/me', {
+            method: 'GET',
+            headers: {
+                authorization: 'c74b18e0-4a10-4f3c-8fc2-aaf6d9f451dc'
+            }
+        })
+        .then((res) => {
+            if (res.ok) {
+                return res.json()
+            } else {
+                return res.status
+            }
+
+        })
+        .catch((err) => console.log(err));
+    return promise;
+};
+
+
+
+export function editProfile(myName, description) {
+    fetch('https://nomoreparties.co/v1/plus-cohort-9/users/me', {
+        method: 'PATCH',
+        headers: {
+            authorization: 'c74b18e0-4a10-4f3c-8fc2-aaf6d9f451dc',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            name: myName,
+            about: description
+        })
+    });
+};
